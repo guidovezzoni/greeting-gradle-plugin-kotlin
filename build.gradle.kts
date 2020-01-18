8586c784cb5f5ff2df6810d591a30b76022d99ea
Guido Vezzoni <guido.vezzoni.development@gmail.com>
2020-01-18 09:19:58 +0100

    v 1.0.0 published

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,8 @@
 plugins {
     java
+    id("java-gradle-plugin")
+    id ("com.gradle.plugin-publish") version "0.10.1"
 }
-
-group = "com.guidovezzoni.greeting"
-version = "1.0.0"
 
 repositories {
     mavenCentral()
@@ -15,4 +14,22 @@ dependencies {
 
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_8
+}
+
+gradlePlugin {
+    plugins {
+        create("greetingsPlugin") {
+            id = "com.guidovezzoni.greetingkotlin"
+            version = "1.0.0"
+            displayName = "Greeting Plugin"
+            description = "Template for people to start their own plugin adventure - in Kotlin"
+            implementationClass = "com.guidovezzoni.greetingkotlin.GreetingPlugin"
+        }
+    }
+}
+
+pluginBundle {
+    website = "https://github.com/guidovezzoni/greeting-gradle-plugin-kotlin"
+    vcsUrl = "https://github.com/guidovezzoni/greeting-gradle-plugin-kotlin.git"
+    tags = listOf("example", "template", "kotlin")
 }
